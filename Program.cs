e6f26665dd35ba5cd17ab6024273979448a98dbf
polidow <gabriel.polido01@etec.sp.gov.br>
2024-03-27 17:18:41 -0300

    eu sou a dori

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Doriii");
+
+Console.WriteLine("escreva seu texto aqui:");
+string corro=Console.ReadLine()!;
+Console.WriteLine(corro.Replace("a","aaa").Replace("e","eee").Replace("i","iii").Replace("o","ooo").Replace("u","uuu"));
